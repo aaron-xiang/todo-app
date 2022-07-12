a309b7639354ddd50ad970834c0f2271a440f4d2
Aaron Xiang <aaron.xiang@gmail.com>
2022-07-11 17:19:25 -0700

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+function Footer() {
+  return (
+    <footer>
+      <p></p>
+      <Link to='/about'>About</Link>
+    </footer>
+  )
+}
+
+export default Footer
